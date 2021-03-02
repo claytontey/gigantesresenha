4777273e2dba49e9c0b11737e99f9480536b597a
Clayton <claytontey@gmail.com>
2021-03-02 10:44:44 -0300

    Up-fevereiro

--- gig_app.py
+++ gig_app.py
@@ -92,7 +92,7 @@ if choice == 'Janeiro-21':
     btn_Melhor_mes = st.sidebar.button("Melhor jogador do mês")
     if btn_Melhor_mes:
         st.write('Melhores jogadorores de Janeiro: ',maior[maior[df.columns[6]] >= 6][:3])
-        )
+    
 
     btn_Pior_mes = st.sidebar.button("Pior jogador do mês")
     if btn_Pior_mes:
